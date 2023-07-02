b7597082c17535b9206793a972b23017127ed123
Nguyen Van Long <102010385+nguyenlong2i2@users.noreply.github.com>
2023-07-02 21:35:57 +0700

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 
@@ -30,4 +31,5 @@ urlpatterns = [
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += staticfiles_urlpatterns()
 
